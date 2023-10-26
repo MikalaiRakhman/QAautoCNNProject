cf79d3975722371ccb2a4cf3a3069ec81d3652d0
MikalaiRakhman <rakhmanmikalai@gmail.com>
2023-10-26 22:10:08 +0200

    четвертый день

---
+++ QAautoCNNProject/CNNHomePage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace QAautoCNNProject
+{
+    internal class CNNHomePage
+    {
+        IWebDriver _webDriver;
+        const string HOME_PAGE_URL = "https://edition.cnn.com/";
+        public CNNHomePage(IWebDriver _driver) 
+        {
+            _webDriver = _driver;
+            _webDriver.Url = HOME_PAGE_URL;
+            _webDriver.Manage().Window.Maximize();
+
+            WebDriverWait wait = new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5));
+            wait.Until(x => x.FindElement(By.XPath("//*[@id=\"onetrust-accept-btn-handler\"]")));
+
+            // Accept all coockies
+            _webDriver.FindElement(By.XPath("//*[@id=\"onetrust-accept-btn-handler\"]")).Click();
+            
+            
+        }
+
+        public bool CheckPageLink(string keyWord)
+        {
+            string xPath = $"//a[@class='header__nav-item-link'][contains(@data-zjs-component_text, '{keyWord}')]";
+            IWebElement menuItem = _webDriver.FindElement(By.XPath(xPath));
+            menuItem.Click();
+            string title = _webDriver.Title;
+            string newUrl = _webDriver.Url;
+            _webDriver.Navigate().Back();
+
+            return title.Contains(keyWord) && newUrl.Contains(keyWord.ToLower());
+        }
+
+    }
+}
--- QAautoCNNProject/Program.cs
+++ QAautoCNNProject/Program.cs
@@ -1,6 +1,6 @@
 ﻿using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium;
-using System.Runtime.CompilerServices;
+
 
 namespace QAautoCNNProject
 {
@@ -9,51 +9,21 @@ namespace QAautoCNNProject
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
-            string mainPage = "https://edition.cnn.com/";
-            driver.Url = mainPage;
 
-            Thread.Sleep(2000);
-
-            // Accept all coockies
-            IWebElement acceptAllCoockiesButtom = driver.FindElement(By.XPath("//*[@id=\"onetrust-accept-btn-handler\"]"));
-            acceptAllCoockiesButtom.Click();
-            Thread.Sleep(2000);
+            CNNHomePage homePage = new CNNHomePage(driver);
 
 
-            static void CheckMainPageMenuItem(IWebDriver driver, string keyWord)
-            {
-                string xPath = $"//a[@class='header__nav-item-link'][contains(@data-zjs-component_text, '{keyWord}')]";
-                IWebElement menuItem = driver.FindElement(By.XPath(xPath));
-                menuItem.Click();
-                string title = driver.Title;
-                // test 1
-                Console.WriteLine(title);
-                Console.WriteLine("Test case 1. This page is opened:");
-                Console.WriteLine(title.Contains(keyWord));
-                // test 2
-                string newUrl = driver.Url;
-                Console.WriteLine($"Test case 2. Link is valid: {newUrl}");
-                Console.WriteLine(newUrl.Contains(keyWord.ToLower()));
-                Console.WriteLine();
-                //return to Home Page
-                driver.Url = "https://edition.cnn.com/";
-            }
-            
-            CheckMainPageMenuItem(driver, "US");
-            CheckMainPageMenuItem(driver, "World");
-            CheckMainPageMenuItem(driver, "Politics");
-            CheckMainPageMenuItem(driver, "Business");
-            CheckMainPageMenuItem(driver, "Opinion");
-            CheckMainPageMenuItem(driver, "Health");
-            CheckMainPageMenuItem(driver, "Entertainment");
-            CheckMainPageMenuItem(driver, "Style");
-            CheckMainPageMenuItem(driver, "Travel");
-            CheckMainPageMenuItem(driver, "Sports"); // bug found
-            CheckMainPageMenuItem(driver, "Video");
-
-            
-
+            bool USCheckResult = homePage.CheckPageLink("US");
+            bool worldCheckResult = homePage.CheckPageLink("World");
+            bool politicsCheckResult = homePage.CheckPageLink("Politics");
+            bool businessCheckResult = homePage.CheckPageLink("Business");
+            bool opinionCheckResult = homePage.CheckPageLink("Opinion");
+            bool healthCheckResult = homePage.CheckPageLink("Health");
+            bool entertainmentCheckResult = homePage.CheckPageLink("Entertainment");
+            bool styleCheckResult = homePage.CheckPageLink("Style");
+            bool travelCheckResult = homePage.CheckPageLink("Travel");
+            bool sportsCheckResult = homePage.CheckPageLink("Sports"); // bug?
+            bool videoCheckResult = homePage.CheckPageLink("Video");
            
 
             driver.Close();
